80519915a0d9b1e7e50bcd06cc9dbb3231ae2fbc
Tejas <tejaskothari03@gmail.com>
2025-03-19 20:45:05 +0000

    [config] Reverted fucked shit

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,6 @@ const nextConfig: NextConfig = {
     // This allows production builds to complete even if there are ESLint errors.
     ignoreDuringBuilds: true,
   },
-  output: "export", // Enables static export
-  distDir: "out", // Export directory
-  basePath: "/tejas-portfolio", // Use the repository name
-  assetPrefix: "/tejas-portfolio/",
 };
 
 export default nextConfig;
