cd5918fc8fecfeb48e694f4bad69bad63c529b67
Tejas <tejaskothari03@gmail.com>
2025-03-19 20:21:24 +0000

    [config] Deployment changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // This allows production builds to complete even if there are ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  output: "export", // Enables static export
+  distDir: "out", // Export directory
+  basePath: "/tejas-portfolio", // Use the repository name
+  assetPrefix: "/tejas-portfolio/",
 };
 
 export default nextConfig;
